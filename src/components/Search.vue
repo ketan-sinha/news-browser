230dbf758c37ce26b408400bcee6065f28f1c8aa
Ketan Sinha <ktn1997@gmail.com>
2021-04-15 21:27:33 -0500

    Added language, topic, and from and to filters

--- src/components/Search.vue
+++ src/components/Search.vue
@@ -1,22 +1,45 @@
 <template>
   <div class="w-full mx-6">
     <form v-on:submit.prevent="submitSearch">
-      <input v-model="options.query" class="w-full appearance-none border-2 border-gray-400 focus:border-black max-w-sm md:max-w-3xl xl:max-w-6xl h-12 rounded-full px-4" type="text" placeholder="Search articles..."/>
-      <div class="inline ml-4">
-        <svg xmlns="http://www.w3.org/2000/svg" class="inline align-middle stroke-current" width="24" height="24" viewBox="0 0 24 24" stroke-width="1.5" stroke="#000000" fill="none" stroke-linecap="round" stroke-linejoin="round">
-          <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
-          <circle cx="12" cy="12" r="9" />
-          <line x1="3.6" y1="9" x2="20.4" y2="9" />
-          <line x1="3.6" y1="15" x2="20.4" y2="15" />
-          <path d="M11.5 3a17 17 0 0 0 0 18" />
-          <path d="M12.5 3a17 17 0 0 1 0 18" />
-        </svg>
-        <select class="inline" v-model="options.lang">
-          <option disabled value="">Language</option>
-          <option class="text-sm" v-for="option in lang_options" :key="option" :value="option.value">
-            {{ option.text }} [{{ option.value }}]
-          </option>
-        </select>
+      <input v-model.lazy="options.q" class="w-full appearance-none border-2 border-gray-400 focus:border-black max-w-sm md:max-w-3xl xl:max-w-6xl h-12 rounded-full px-4" type="text" placeholder="Search articles..."/>
+      <div class="mt-4">
+        <div class="inline">
+          <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 inline align-middle" viewBox="0 0 20 20" fill="currentColor">
+            <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zM4.332 8.027a6.012 6.012 0 011.912-2.706C6.512 5.73 6.974 6 7.5 6A1.5 1.5 0 019 7.5V8a2 2 0 004 0 2 2 0 011.523-1.943A5.977 5.977 0 0116 10c0 .34-.028.675-.083 1H15a2 2 0 00-2 2v2.197A5.973 5.973 0 0110 16v-2a2 2 0 00-2-2 2 2 0 01-2-2 2 2 0 00-1.668-1.973z" clip-rule="evenodd" />
+          </svg>
+          <select class="inline text-sm p-2 ml-2 rounded-full" v-model="options.lang">
+            <option disabled value="">Language</option>
+            <option class="text-sm" v-for="option in lang_options" :key="option" :value="option.value">
+              [{{ option.value }}] {{ option.text }} 
+            </option>
+          </select>
+        </div>
+        <div class="inline ml-6">
+          <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 inline" viewBox="0 0 20 20" fill="currentColor">
+            <path fill-rule="evenodd" d="M2 5a2 2 0 012-2h8a2 2 0 012 2v10a2 2 0 002 2H4a2 2 0 01-2-2V5zm3 1h6v4H5V6zm6 6H5v2h6v-2z" clip-rule="evenodd" />
+            <path d="M15 7h1a2 2 0 012 2v5.5a1.5 1.5 0 01-3 0V7z" />
+          </svg>
+          <select class="inline text-sm p-2 ml-2 rounded-full" v-model="options.topic">
+            <option disabled value="">Topic</option>
+            <option class="text-sm" v-for="topic in topic_options" :key="topic" :value="topic">
+              {{ topic }} 
+            </option>
+          </select>
+        </div>
+        <div class="inline ml-6">
+          <div class="inline">
+            <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 inline" viewBox="0 0 20 20" fill="currentColor">
+              <path fill-rule="evenodd" d="M6 2a1 1 0 00-1 1v1H4a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V6a2 2 0 00-2-2h-1V3a1 1 0 10-2 0v1H7V3a1 1 0 00-1-1zm0 5a1 1 0 000 2h8a1 1 0 100-2H6z" clip-rule="evenodd" />
+            </svg>
+            <input v-model="options.from" class="inline ml-2 text-sm p-2 rounded-full" type="date">
+          </div>
+          <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 ml-1 inline" viewBox="0 0 20 20" fill="currentColor">
+            <path fill-rule="evenodd" d="M3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clip-rule="evenodd" />
+          </svg>
+          <div class="inline ml-1">
+            <input v-model="options.to" class="inline text-sm p-2 rounded-full" type="date">
+          </div>
+        </div>
       </div>
     </form>
   </div>
@@ -87,22 +110,41 @@ export default {
       { text: 'اردو', value: 'ur' },
       { text: 'Tiếng Việt', value: 'vi' }
     ]
+
+    const topic_options = [
+      'news',
+      'sport',
+      'tech',
+      'world',
+      'finance',
+      'politics',
+      'business',
+      'economics',
+      'entertainment',
+      'beauty',
+      'gaming'
+    ]
+
     const options = reactive ({
-      query: '',
-      lang: ''
+      q: '',
+      lang: '',
+      topic: '',
+      from: '',
+      to: '',
+      page_size: 24
     })
 
     const store = useStore()
 
     function submitSearch() {
-      if (options.query) {
+      if (options.q) {
         store.dispatch('setSearchOptions', options)
         context.emit('search-news');
-        options.query = '';
       }
     }
 
     return {
+      topic_options,
       lang_options,
       options,
       submitSearch
