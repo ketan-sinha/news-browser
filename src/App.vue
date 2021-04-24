d3618da65cde64cc596a1a6cbb147f6025d573bf
Ketan Sinha <ktn1997@gmail.com>
2021-04-24 01:04:27 -0500

    Basic app info header

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,7 @@
 <template>
+  <div class="absolute top-0 ml-4 my-1 text-xs">
+    browser for the <a class="text-blue-700 underline" href="https://free-docs.newscatcherapi.com/">Newscatcher API</a> | built by ketan sinha | <a class="text-blue-700 underline" href="https://github.com/ketan-sinha/news-browser">github</a>
+  </div>
   <h1 class="text-3xl font-semibold -mt-8 mb-2 font-mono">newscatcherapi browser</h1>
   <Feed/>
 </template>
