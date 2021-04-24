f72e8788c59e227188696227a6d897d0ba483ccd
Ketan Sinha <ktn1997@gmail.com>
2021-04-23 22:57:59 -0500

    Adjusted title

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <h1 class="text-2xl font-semibold -mt-8 mb-2 font-mono">newscatcherapi browser</h1>
+  <h1 class="text-3xl font-semibold -mt-8 mb-2 font-mono">newscatcherapi browser</h1>
   <Feed/>
 </template>
 
