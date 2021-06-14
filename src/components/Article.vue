e7f3683c9a845bb3e5aaea552b7a2127f11cc19c
Ketan Sinha <ktn1997@gmail.com>
2021-06-14 11:43:31 -0500

    Show twitter SVG only if twitter_account is not empty

--- src/components/Article.vue
+++ src/components/Article.vue
@@ -19,7 +19,7 @@
       </div>
     </div>
     <div class="mx-2 my-3 pt-2 h-1/6 border-t">
-      <svg xmlns="http://www.w3.org/2000/svg" class="inline" :title="twitter_account" :v-if="twitter_account" width="20" height="20" viewBox="0 0 24 24" stroke-width="1.5" stroke="#00abfb" fill="none" stroke-linecap="round" stroke-linejoin="round">
+      <svg v-if="twitter_account" xmlns="http://www.w3.org/2000/svg" class="inline" :title="twitter_account" :v-if="twitter_account" width="20" height="20" viewBox="0 0 24 24" stroke-width="1.5" stroke="#00abfb" fill="none" stroke-linecap="round" stroke-linejoin="round">
         <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
         <path d="M22 4.01c-1 .49 -1.98 .689 -3 .99c-1.121 -1.265 -2.783 -1.335 -4.38 -.737s-2.643 2.06 -2.62 3.737v1c-3.245 .083 -6.135 -1.395 -8 -4c0 0 -4.182 7.433 4 11c-1.872 1.247 -3.739 2.088 -6 2c3.308 1.803 6.913 2.423 10.034 1.517c3.58 -1.04 6.522 -3.723 7.651 -7.742a13.84 13.84 0 0 0 .497 -3.753c-.002 -.249 1.51 -2.772 1.818 -4.013z" />
       </svg>
